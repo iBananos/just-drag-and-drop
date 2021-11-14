9071d8fc4ea073b4fe14cf93f765a61b77a4512e
Jeremy HIRTH <jer0622@yahoo.fr>
2021-11-14 22:26:00 +0100

    Rectification dernier commit #8 #9

--- backend/index.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const app = express();
-
-const PORT = process.env.PORT || 4000;
-
-app.get("/", (req, res) => res.send("Hello from server!"));
-
-app.listen(PORT, () => console.log('Server is running on port ' + PORT));
