5ccbe5de8ad3a2af468b048c278f02b775e97c9c
iBananos <56481216+iBananos@users.noreply.github.com>
2021-11-20 04:08:29 +0100

    history page design #17 #18

---
+++ site-web/react-ts-app/src/components/ViewHistory.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const ViewHistory = () => {
+
+    function loadAnalyze(){
+        var title = (document.getElementById("title")as HTMLInputElement).value;
+        var date = (document.getElementById("date")as HTMLInputElement).value;
+        var database = (document.getElementById("database")as HTMLInputElement).value;
+        if(title !== ""){
+
+        }
+
+    }
+
+    return (
+        <div id="ViewHistory" className="ViewHistory">
+            <button className="loadButton" id="loadButton" onClick={loadAnalyze}>Load analyze</button>
+            <p id="title"></p>
+            <p id="date"></p>
+            <p id="database"></p>
+        </div>
+    );
+};
+
+export default ViewHistory;
--- site-web/react-ts-app/src/pages/History.tsx
+++ site-web/react-ts-app/src/pages/History.tsx
@@ -1,15 +1,39 @@
 import BarreLaterale from "../components/BarreLaterale";
+import HistoryCell from "../components/HistoryCell";
 import Navigation from "../components/Navigation";
+import ViewHistory from "../components/ViewHistory";
 
 const History = () =>  {
     return (
         
         <div className="History">
+            
+            <div className="viewHistory">
+                <h1 className="title">History page</h1>
+                <HistoryCell name="LE NOMfdsfdsfdsqfdsqfsdqfsdfdsqfds" date=" AVANT HIERfsfsfsqfsqfsqfqs" nomBase="BASE 1fsfsqfsqfsdqfsqfsdfsq"/>
+                <HistoryCell name="Bonjour" date=" demain" nomBase="BASE 2"/>
+                <HistoryCell />
+                <HistoryCell />
+                <HistoryCell />
+                <HistoryCell />
+                <HistoryCell />
+                <HistoryCell />
+                <HistoryCell />
+                <HistoryCell />
+                <HistoryCell />
+                <HistoryCell />
+                <HistoryCell />
+                <HistoryCell />
+                <HistoryCell />
+                <HistoryCell />
+                <HistoryCell />
+                <HistoryCell />
+                <HistoryCell />
+                <HistoryCell />
+            </div>
+            <ViewHistory />
             <BarreLaterale />
             <Navigation />
-            <div className="view">
-                <h1 className="title">History page</h1>
-            </div>
         </div>
     )
 }
