422fdc5ff9be667d96b59ce8a4488f920c144c59
NicolasDemolin <66180067+NicolasDemolin@users.noreply.github.com>
2021-12-13 17:45:16 +0100

    Update autoselectionalgo.py
    correction bug excel et csv

--- backend/python/autoselectionalgo.py
+++ backend/python/autoselectionalgo.py
@@ -29,10 +29,13 @@ def parse_data(filename):
 
     if extension == "csv" :
             # Assume that the user uploaded a CSV or TXT file
-        df = pd.read_csv(filename,index_col=0, delimiter=',', encoding="utf-8")
-    elif extension == 'xlsx' :
+        try:
+            df = pd.read_csv(filename,index_col=0, delimiter=',', encoding="utf-8")
+        except:
+            df = pd.read_csv(filename, delimiter=',', encoding="utf-8")
+    elif extension == 'xlsx' or extension == 'xls':
             # Assume that the user uploaded an excel file
-        df = pd.read_excel(filename,index_col=0,encoding="utf-8")
+        df = pd.read_excel(filename,index_col=0)
     elif extension == 'txt' or extension == 'tsv' :
             # Assume that the user upl, delimiter = r'\s+'oaded an excel file
         df = pd.read_csv(filename, delimiter = r'\s+',index_col=0, encoding="utf-8")
@@ -40,7 +43,7 @@ def parse_data(filename):
         df = pd.read_json(filename)
     else :
         print("There was an error while processing this file")
-
+    
     return df
 
 
