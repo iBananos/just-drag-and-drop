e042228eba3472ff46569ae517c7958cc6e120b2
iBananos <ralph.el-chalfoun@etu.unice.fr>
2022-01-11 18:40:25 +0100

    bugfix colonnes espacées datavisu

--- backend/src/controllers/dataVisu.ts
+++ backend/src/controllers/dataVisu.ts
@@ -16,7 +16,7 @@ export const  parameters : RequestHandler = (req : Request, res : Response, next
     let targetBase = Utils.default.findEncryptedFile(req.body.userId, "uploads/" + req.body.userId + "/database/", req.body.database);
     let filename = "uploads/" + req.body.userId + "/database/" + targetBase;
     let extension = req.body.database.split(".")[1];
-    exec('python python_script/datavisu.py "' + filename + '" ' + extension + ' ' + req.body.firstOne + ' ' + req.body.secondOne + ' ' + req.body.thirdOne + ' ' + req.body.sample + ' false ' + aesCipher.getKey() + ' ' + aesCipher.getToEncrypt(), (error:any, stdout:any, stderr:any) => {
+    exec('python python_script/datavisu.py "' + filename + '" ' + extension + ' "' + req.body.firstOne + '" "' + req.body.secondOne + '" "' + req.body.thirdOne + '" ' + req.body.sample + ' false ' + aesCipher.getKey() + ' ' + aesCipher.getToEncrypt(), (error:any, stdout:any, stderr:any) => {
         if (error) {
             console.error(`error: ${error.message}`);
             return;
@@ -33,7 +33,7 @@ export const  parameters : RequestHandler = (req : Request, res : Response, next
 export const  parametersDemo : RequestHandler = (req : Request, res : Response, next : NextFunction) => {
     let filename = "uploads/" + "demo" + "/database/" + req.body.database;
     let extension = req.body.database.split(".")[1];
-    exec('python python_script/datavisu.py "' + filename + '" ' + extension + ' ' + req.body.firstOne + ' ' + req.body.secondOne + ' ' + req.body.thirdOne + ' ' + req.body.sample + ' ' + 'true' , (error:any, stdout:any, stderr:any) => {
+    exec('python python_script/datavisu.py "' + filename + '" ' + extension + ' "' + req.body.firstOne + '" "' + req.body.secondOne + '" "' + req.body.thirdOne + '" ' + req.body.sample + ' ' + 'true' , (error:any, stdout:any, stderr:any) => {
         if (error) {
             console.error(`error: ${error.message}`);
             return;
